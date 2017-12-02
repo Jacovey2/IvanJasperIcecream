b7bb083684c6b67564db5976a2f8d0659a531f30
Jacovey <jaspercovey@gmail.com>
2017-12-02 00:12:08 -0500

    update

--- IceCreamProject/src/Cone.java
+++ IceCreamProject/src/Cone.java
@@ -3,42 +3,16 @@ import java.awt.Polygon;
 import java.util.List;
 
 public class Cone {
-	int x;
-	int y;
-	int length;
-	String type;
-	List<Scoop> scoops;
+	public int x;
+	public int y;
 	
 	public Cone(int xPos, int yPos, int Length, Graphics g) {
 		x=xPos;
 		y=yPos;
-		length=Length;
-		type="Cone";
 		Polygon p= new Polygon();
 		p.addPoint(xPos, yPos);
 		p.addPoint(xPos+100, yPos);
 		p.addPoint(xPos+50, yPos+150);
 		g.drawPolygon(p);
-	}
-	
-	public Cone(int xPos, int yPos, int Length, String Type, Graphics g) {
-		x=xPos;
-		y=yPos;
-		length=Length;
-		type=Type;
-		Polygon p= new Polygon();
-		if (Type.equals("cone")) {
-			p.addPoint(xPos, yPos);
-			p.addPoint(xPos+100, yPos);
-			p.addPoint(xPos+50, yPos+150);
-			g.drawPolygon(p);
-		}
-		if (Type.equals("cup")) {
-			p.addPoint(xPos, yPos);
-			p.addPoint(xPos+100, yPos);
-			p.addPoint(xPos+15, yPos+50);
-			p.addPoint(xPos+85, yPos+50);
-			g.drawPolygon(p);
-		}
 	}
 }
--- IceCreamProject/src/GUI.java
+++ IceCreamProject/src/GUI.java
@@ -1,13 +1,24 @@
 import java.awt.*;
 import java.applet.*;
+import java.util.Scanner;
 
 public class GUI extends Applet{
+	public double subTotal;
+	public int length;
+	
 	public void init() {
 		setSize(new Dimension(500,500));
 	}
 	
 	public void paint(Graphics g) {
-		Cone c= new Cone(200,500,0);
+		Scanner scan= new Scanner(System.in);
+		Cone c= new Cone(200,500,0,g);
+		
+		//Get how many scoops
+		length = scan.nextInt();
+		for(int i=0; i<length; i++) {
+			
+		}
 	}
 	
 }
