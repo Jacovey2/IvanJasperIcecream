ccdcf5a723364021a5298b8ce598d779c9e673f0
ivfithian <ivfithian@packer.edu>
2017-12-04 14:09:50 -0500

    hello

--- IceCreamProject/src/Mango.java
+++ IceCreamProject/src/Mango.java
@@ -4,7 +4,7 @@ public class Mango extends Scoop{
 
 	public Mango(int xPos, int yPos, Graphics g) {
 		super(xPos, yPos, new Color(233,186,0), g);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
--- IceCreamProject/src/Strawberry.java
+++ IceCreamProject/src/Strawberry.java
@@ -4,7 +4,7 @@ public class Strawberry extends Scoop{
 
 	public Strawberry(int xPos, int yPos, Graphics g) {
 		super(xPos, yPos, new Color(233,155,155), g);
-		// TODO Auto-generated constructor stub
+
 	}
 
 }
