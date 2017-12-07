2c8158c7393f415a9d18d7a6da9e584b55b59fdb
Jacovey <jaspercovey@gmail.com>
2017-12-07 12:02:30 -0500

    update

--- IceCreamProject/src/CookiesNCream.java
+++ IceCreamProject/src/CookiesNCream.java
@@ -6,10 +6,17 @@ public class CookiesNCream extends Scoop{
 		super(xPos, yPos, topping, new Color(255, 240, 220), g);
 		Random r = new Random();
 		int numChuncks = r.nextInt(10)+5;
-		int completed=0;
-		while (completed<numChuncks) {
-			ra
+		int numCompleted=0;
+		while (numCompleted<numChuncks) {
+			int x = r.nextInt(100)+xPos;
+			int y = r.nextInt(100)+yPos;
+			if (Math.sqrt(Math.pow(x-(xPos+50), 2)+Math.pow(y-(yPos+50), 2))<50.0) {
+				Polygon p= new Polygon();
+				p.addPoint(x-3, y+r.nextInt(5)-2);
+				p.addPoint(x+3, y);
+			}
 		}
+		
 		
 	}
 }
