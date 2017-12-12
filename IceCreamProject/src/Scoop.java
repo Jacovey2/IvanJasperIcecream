fe71fea7f99419abe3fc3e5e43ea027b170e4965
Jacovey <jaspercovey@gmail.com>
2017-12-12 10:53:43 -0500

    push

--- IceCreamProject/src/Scoop.java
+++ IceCreamProject/src/Scoop.java
@@ -8,6 +8,22 @@ public class Scoop {
 	protected Color color;
 	protected Graphics graphics;
 
+	public Scoop(int xPos, int yPos, Color C, Graphics g) {
+		price = 1.0;
+		x = xPos;
+		y = yPos;
+		color = C;
+		graphics = g;
+		g.setColor(color);
+		g.fillOval(x, y, 100, 100);
+		g.fillOval(x, y + 75, 25, 25);
+		g.fillOval(x + 20, y + 75, 25, 25);
+		g.fillOval(x + 40, y + 75, 25, 25);
+		g.fillOval(x + 60, y + 75, 25, 25);
+		g.fillOval(x + 75, y + 75, 25, 25);
+		g.setColor(Color.black);
+	}
+	
 	public Scoop(int xPos, int yPos, String topping, Color C, Graphics g) {
 		price = 1.0;
 		x = xPos;
