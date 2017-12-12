7250985619d5f5eb9f29dfaafa8eddabe91373b7
Jacovey <jaspercovey@gmail.com>
2017-12-12 11:15:45 -0500

    update

--- IceCreamProject/src/Scoop.java
+++ IceCreamProject/src/Scoop.java
@@ -8,6 +8,22 @@ public class Scoop {
 	protected Color color;
 	protected Graphics graphics;
 
+	public Scoop(int xPos, int yPos, Color C, Graphics g) {
+		price = 1.0;
+		x = xPos;
+		y = yPos;
+		color = C;
+		graphics = g;
+		g.setColor(color);
+		g.fillOval(x, y, 100, 100);//main scoop 
+		g.fillOval(x, y + 75, 25, 25);//creates smaller circles at the bottom of scoop to give nicer look
+		g.fillOval(x + 20, y + 75, 25, 25);
+		g.fillOval(x + 40, y + 75, 25, 25);
+		g.fillOval(x + 60, y + 75, 25, 25);
+		g.fillOval(x + 75, y + 75, 25, 25);
+		g.setColor(Color.black);
+	}
+	
 	public Scoop(int xPos, int yPos, String topping, Color C, Graphics g) {
 		price = 1.0;
 		x = xPos;
@@ -15,8 +31,6 @@ public class Scoop {
 		color = C;
 		graphics = g;
 		g.setColor(color);
-		g.fillOval(x, y, 100, 100);
-		g.fillOval(x, y + 75, 25, 25);
 		g.fillOval(x, y, 100, 100);//main scoop 
 		g.fillOval(x, y + 75, 25, 25);//creates smaller circles at the bottom of scoop to give nicer look
 		g.fillOval(x + 20, y + 75, 25, 25);
@@ -25,5 +39,10 @@ public class Scoop {
 		g.fillOval(x + 75, y + 75, 25, 25);
 		g.setColor(Color.black);
 		Topping t = new Topping(x,y,topping,g);
+	}
+
+	private Color RandomColor() {//random color generator
+		Random r = new Random();
+		return new Color(r.nextInt(254), r.nextInt(254), r.nextInt(254));
 	}
 }
